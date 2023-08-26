d3dccab95c1c5995900b8bc30661204af962e699
Daniel Jatnieks <jatnieks@pobox.com>
2023-08-25 19:03:51 -0700

    Prefer OSS handling of die in DefaultFSErrorHandler over changed made in STAR-13 and STAR-1502

--- src/java/org/apache/cassandra/service/DefaultFSErrorHandler.java
+++ src/java/org/apache/cassandra/service/DefaultFSErrorHandler.java
@@ -50,14 +50,11 @@ public class DefaultFSErrorHandler implements FSErrorHandler
 
         switch (DatabaseDescriptor.getDiskFailurePolicy())
         {
+            case die:
             case stop_paranoid:
                 // exception not logged here on purpose as it is already logged
                 logger.error("Stopping transports as disk_failure_policy is " + DatabaseDescriptor.getDiskFailurePolicy());
                 StorageService.instance.stopTransports();
-                break;
-
-            case die:
-                JVMStabilityInspector.killCurrentJVM(e, false);
                 break;
         }
     }
@@ -70,6 +67,7 @@ public class DefaultFSErrorHandler implements FSErrorHandler
 
         switch (DatabaseDescriptor.getDiskFailurePolicy())
         {
+            case die:
             case stop_paranoid:
             case stop:
                 // exception not logged here on purpose as it is already logged
@@ -97,9 +95,6 @@ public class DefaultFSErrorHandler implements FSErrorHandler
                     if (directory != null)
                         Keyspace.removeUnreadableSSTables(directory);
                 }
-                break;
-            case die:
-                JVMStabilityInspector.killCurrentJVM(e, false);
                 break;
             case ignore:
                 // already logged, so left nothing to do
