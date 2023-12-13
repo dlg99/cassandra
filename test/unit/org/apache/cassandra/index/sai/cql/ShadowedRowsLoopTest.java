c3b354760b1a33ab87120986a9ac2b6bf820e611
Andrey Yegorov <andrey.yegorov@datastax.com>
2023-12-13 14:10:31 -0800

    prepareDataMixedDelsInMemoryAndDisk to delete some of the live rows

--- test/unit/org/apache/cassandra/index/sai/cql/ShadowedRowsLoopTest.java
+++ test/unit/org/apache/cassandra/index/sai/cql/ShadowedRowsLoopTest.java
@@ -119,7 +119,7 @@ public class ShadowedRowsLoopTest extends VectorTester
         prepareDataOnDisk();
 
         // delete some, don't flush
-        for (int i = liveVectorsNum; i < liveVectorsNum + vectorCount / 2; i += 2)
+        for (int i = liveVectorsNum - vectorCount / 2; i < liveVectorsNum + vectorCount / 2; i += 2)
         {
             execute("DELETE FROM %s WHERE pk = ?", i);
         }
@@ -201,7 +201,7 @@ public class ShadowedRowsLoopTest extends VectorTester
     public void shadowedLoopTest(int limit) throws Throwable
     {
         resetMetrics();
-        
+
         QueryController.allowSpeculativeLimits = false;
         search(queryVector, limit);
         Metrics resultNoSp = getMetrics();
